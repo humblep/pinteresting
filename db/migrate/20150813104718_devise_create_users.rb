59c9d8c1d995e5bac0a523b98abcce697477940c
Roberto Gamero <hprobert96@gmail.com>
2015-08-21 11:00:37 -0700

    Add pins/users association

--- db/migrate/20150813104718_devise_create_users.rb
+++ db/migrate/20150813104718_devise_create_users.rb
@@ -1,5 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
+    drop_table :users
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -31,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      t.timestamps
     end
 
     add_index :users, :email,                unique: true
